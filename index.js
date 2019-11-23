3070dd8fbf96c1a1508ab1fb197fd7870d0b9102
Yuri Zakharov <yuri.zaharov@gmail.com>
2019-11-23 16:23:38 +0300

    init project

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js Server!');
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+})
+
+import Vue from 'vue'
+import App from './App.vue'
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
